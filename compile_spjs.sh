5fb6ddc917c81ce978a888416c4b03899a917e61
John Lauer <jlauer@zipwhip.com>
2015-07-19 13:43:22 -0700

    New build script

---
+++ compile_spjs.sh
@@ -0,0 +1,29 @@
+# git submodule init
+# git submodule update
+
+echo "About to cross-compile Serial Port JSON Server"
+if [ "$1" = "" ]; then
+        echo "You need to pass in the version number as the first parameter."
+        exit
+fi
+
+cp -r arduino/tools_linux_64  arduino/tools
+goxc -os="linux" -arch="amd64" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server_$1" -d=.
+rm -rf arduino/tools
+
+cp -r arduino/tools_linux_32  arduino/tools
+goxc -os="linux" -arch="386" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server_$1" -d=.
+rm -rf arduino/tools
+
+cp -r arduino/tools_linux_arm  arduino/tools
+goxc -os="linux" -arch="arm" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server_$1" -d=.
+rm -rf arduino/tools
+
+cp -r arduino/tools_windows  arduino/tools
+goxc -os="windows" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server_$1" -d=.
+rm -rf arduino/tools
+
+cp -r arduino/tools_darwin  arduino/tools
+goxc -os="darwin" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server_$1" -d=.
+rm -rf arduino/tools
+
