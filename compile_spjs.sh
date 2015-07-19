4676388b8ad7d93db66c0fc0688963981db5acdd
John Lauer <jlauer@zipwhip.com>
2015-07-19 14:23:20 -0700

    No version in exe name

--- compile_spjs.sh
+++ compile_spjs.sh
@@ -10,23 +10,23 @@ fi
 cp README.md snapshot/
 
 cp -r arduino/tools_linux_64  arduino/tools
-goxc -os="linux" -arch="amd64" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server_$1" -d=.
+goxc -os="linux" -arch="amd64" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server" -d=.
 rm -rf arduino/tools
 
 cp -r arduino/tools_linux_32  arduino/tools
-goxc -os="linux" -arch="386" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server_$1" -d=.
+goxc -os="linux" -arch="386" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server" -d=.
 rm -rf arduino/tools
 
 cp -r arduino/tools_linux_arm  arduino/tools
-goxc -os="linux" -arch="arm" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server_$1" -d=.
+goxc -os="linux" -arch="arm" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server" -d=.
 rm -rf arduino/tools
 
 cp -r arduino/tools_windows  arduino/tools
-goxc -os="windows" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server_$1" -d=.
+goxc -os="windows" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server" -d=.
 rm -rf arduino/tools
 
 cp -r arduino/tools_darwin  arduino/tools
-goxc -os="darwin" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server_$1" -d=.
+goxc -os="darwin" --include="arduino/hardware,arduino/tools" -n="serial-port-json-server" -d=.
 rm -rf arduino/tools
 
 sudo mkdir "/media/sf_downloads/v$1"
